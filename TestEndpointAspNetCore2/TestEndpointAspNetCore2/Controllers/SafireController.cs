042eb29290faaf10c4e530657266b070c4c86ffe
Carlos M. Hernández <chernandez@visiotechsecurity.com>
2020-07-31 10:33:08 +0200

    First commit

---
+++ TestEndpointAspNetCore2/TestEndpointAspNetCore2/Controllers/SafireController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TestEndpointAspNetCore2.Controllers
+{
+    public class SafireController: Controller
+    {
+        [HttpPost ( "api/testsafire" )]
+        public void PostSafire ( )
+        {
+            var stream = new StreamReader ( Request.Body );
+            var body = stream.ReadToEnd();
+            Console.WriteLine ( body );
+        }
+    }
+}
---
+++ TestEndpointAspNetCore3/TestEndpointAspNetCore3/Controllers/SafireController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestEndpointAspNetCore3.Controllers
+{
+
+    [ApiController]
+    public class SafireController : ControllerBase
+    {
+
+        [HttpPost ( "api/testsafire" )]
+        public async Task PostSafire ()
+        {
+            var stream = new StreamReader ( Request.Body );
+            var body = await stream.ReadToEndAsync();
+            Console.WriteLine ( body );
+        }
+    }
+}
